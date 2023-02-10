293f94d6e46e99b21d5eece7de59c268f888ba79
Miteshdabhi <84114715+Miteshdabhi@users.noreply.github.com>
2023-02-10 18:21:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
 import streamlit as st
 import pandas as pd
-import tabula
+import numpy as np
+from datetime import timedelta
 
 def submit():
-    dfs = tabula.read_pdf(file_path, multiple_tables=False, pages="all")
-    tables = [pd.DataFrame(df) for df in dfs]
-    
-    result = pd.concat(tables)
-    result = result.dropna()
-    result.columns = [' '.join(map(str, x)) for x in zip(list(result.columns), list(result.iloc[0]))]
-    result.drop(result.index[0], inplace=True)
-    result.to_excel(output_name)
-    st.write("Submit button pressed")
+    deliveroo_data = pd.read_csv(file_path)
+    deliveroo_data['Time Submitted'] = pd.to_datetime(deliveroo_data['Time Submitted'], format='%H:%M:%S')
+    deliveroo_data['Date Submitted'] = pd.to_datetime(deliveroo_data['Date Submitted'],format="%Y/%m/%d")
+    deliveroo_data['Time Hour'] = pd.DatetimeIndex(deliveroo_data['Time Submitted']).hour
+    report_dates = []
+    for index,row in deliveroo_data.iterrows():
+        if row['Time Hour'] < 6:
+            yesterday = row['Date Submitted'] - timedelta(days=1)
+            report_dates.append(yesterday.strftime('%Y-%m-%d'))
+        else:
+            today = row['Date Submitted']
+            report_dates.append(today)
+    deliveroo_data['NewDate'] = report_dates
+    df = pd.pivot_table(deliveroo_data,values='Subtotal',index='NewDate',columns=['Restaurant Name'],aggfunc=np.sum).fillna("")
+    df.to_excel(filename.split('.')[0] + '.xlsx')
 
-file_path = st.file_uploader("Select a file", type=["pdf"])
+file_path = st.file_uploader("Select a file", type=["csv"])
 output_name = st.text_input("Output file name:")
 
 if st.button("Submit"):
